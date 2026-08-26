52e462861e9ff18b2e8aa5e3c9dd0f5d2b69300e
nprasath1024 <n.prasath1024@gmail.com>
2026-08-27 01:50:43 +0530

    Commit

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -32,7 +32,6 @@ function Header(props) {
   });
 
   const link = settings.isSplash ? "/splash" : "home";
-
   const [currTheme, setCurrTheme] = useState(props.theme);
 
   function changeTheme() {
@@ -68,7 +67,7 @@ function Header(props) {
         <header className="header">
           <NavLink to={link} tag={Link} className="logo">
             <span style={{ color: theme.text }}></span>
-            {/* Added whiteSpace: "nowrap" to strictly forbid the name from breaking into two lines */}
+            {/* nowrap added exclusively here to fix "Prasath N" splitting */}
             <span className="logo-name" style={{ color: theme.text, whiteSpace: "nowrap" }}>
               {greeting.logo_name}
             </span>
@@ -82,66 +81,34 @@ function Header(props) {
           
           <ul className="menu">
             <li>
-              <NavLink
-                className="homei"
-                to="/home"
-                tag={Link}
-                activeStyle={{ fontWeight: "bold" }}
-                style={{ borderRadius: 5, color: theme.text, whiteSpace: "nowrap" }}
-              >
+              <NavLink className="homei" to="/home" tag={Link} activeStyle={{ fontWeight: "bold" }} style={{ borderRadius: 5, color: theme.text }}>
                 Home
               </NavLink>
             </li>
             <li>
-              <NavLink
-                className="ec"
-                to="/education"
-                tag={Link}
-                activeStyle={{ fontWeight: "bold" }}
-                style={{ borderRadius: 5, color: theme.text, whiteSpace: "nowrap" }}
-              >
+              <NavLink className="ec" to="/education" tag={Link} activeStyle={{ fontWeight: "bold" }} style={{ borderRadius: 5, color: theme.text }}>
                 Education
               </NavLink>
             </li>
             <li>
-              <NavLink
-                className="xp"
-                to="/experience"
-                tag={Link}
-                activeStyle={{ fontWeight: "bold" }}
-                style={{ borderRadius: 5, color: theme.text, whiteSpace: "nowrap" }}
-              >
+              <NavLink className="xp" to="/experience" tag={Link} activeStyle={{ fontWeight: "bold" }} style={{ borderRadius: 5, color: theme.text }}>
                 Experience
               </NavLink>
             </li>
             <li>
-              <NavLink
-                className="projects"
-                to="/projects"
-                tag={Link}
-                activeStyle={{ fontWeight: "bold" }}
-                style={{ borderRadius: 5, color: theme.text, whiteSpace: "nowrap" }}
-              >
+              <NavLink className="projects" to="/projects" tag={Link} activeStyle={{ fontWeight: "bold" }} style={{ borderRadius: 5, color: theme.text }}>
                 Projects
               </NavLink>
             </li>
             <li>
-              <NavLink
-                className="cr"
-                to="/contact"
-                tag={Link}
-                activeStyle={{ fontWeight: "bold" }}
-                style={{ borderRadius: 5, color: theme.text, whiteSpace: "nowrap" }}
-              >
+              <NavLink className="cr" to="/contact" tag={Link} activeStyle={{ fontWeight: "bold" }} style={{ borderRadius: 5, color: theme.text }}>
                 Contact and Resume
               </NavLink>
             </li>
-            {/* Wrapped the button safely inside an <li> tag to lock it into the navigation row */}
-            <li style={{ display: "inline-block" }}>
-              <button {...styles} onClick={changeTheme}>
-                {icon}
-              </button>
-            </li>
+            {/* Reverted exactly to original template structure: no wrapping <li> tag */}
+            <button {...styles} onClick={changeTheme}>
+              {icon}
+            </button>
           </ul>
         </header>
       </div>
